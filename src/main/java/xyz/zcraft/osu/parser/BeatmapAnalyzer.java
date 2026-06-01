116f04fc711fac2458f9fe6daa175108a9e38751
ZayrexDev <zay.rex@qq.com>
2026-06-01 19:54:40 +0800

    Refactor calculateWindowDifficulty to use a temporary file for beatmap processing; improve error handling for FFI exceptions

--- src/main/java/xyz/zcraft/osu/parser/BeatmapAnalyzer.java
+++ src/main/java/xyz/zcraft/osu/parser/BeatmapAnalyzer.java
@@ -8,6 +8,9 @@ import xyz.zcraft.osu.parser.data.beatmap.HitObject;
 import xyz.zcraft.osu.parser.data.beatmap.OsuBeatmap;
 import xyz.zcraft.osu.parser.exception.AnalyzeException;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -55,18 +58,28 @@ public class BeatmapAnalyzer {
 
     public static Pair<Double, Double> calculateWindowDifficulty(OsuBeatmap osuBeatmap, long startTimeMs, long endTimeMs) {
         String slicedOsuString = osuBeatmap.toWindowedBeatmapString(startTimeMs, endTimeMs);
+        try {
+            final Path tempFile = Files.createTempFile("osu-parser-beatmap-temp", ".osu");
+            tempFile.toFile().deleteOnExit();
+            Files.writeString(tempFile, slicedOsuString);
+            try (
+                    desu.life.RosuFFI.Beatmap beatmap = new desu.life.RosuFFI.Beatmap(slicedOsuString.getBytes());
+                    desu.life.RosuFFI.Difficulty diff = new desu.life.RosuFFI.Difficulty();
+                    desu.life.RosuFFI.Performance performance = new RosuFFI.Performance()
+            ) {
+                final double stars = diff.calculate(beatmap).osu.t.stars;
+                final double pp = performance.calculate(beatmap).osu.t.pp;
 
-        try (
-                desu.life.RosuFFI.Beatmap beatmap = new desu.life.RosuFFI.Beatmap(slicedOsuString.getBytes());
-                desu.life.RosuFFI.Difficulty diff = new desu.life.RosuFFI.Difficulty();
-                desu.life.RosuFFI.Performance performance = new RosuFFI.Performance()
-        ) {
-            final double stars = diff.calculate(beatmap).osu.t.stars;
-            final double pp = performance.calculate(beatmap).osu.t.pp;
-            return new ImmutablePair<>(stars, pp);
-        } catch (Exception e) {
+                Files.deleteIfExists(tempFile);
+                return new ImmutablePair<>(stars, pp);
+            } catch (RosuFFI.FFIException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
             throw new RuntimeException("Failed to calculate window difficulty", e);
         }
+
+
     }
 
     public static List<Long> extractTimestamps(OsuBeatmap beatmap) {
